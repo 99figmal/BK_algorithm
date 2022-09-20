d2c83b199bcbe7c47e7f82b7b260fbb2c53d796a
99figmal <109018252+99figmal@users.noreply.github.com>
2022-09-21 00:30:13 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    
+    while(a>0)
+    {
+        scanf("%d %d", &a, &b);
+        
+        if(a == 0) break;
+        
+        printf("%d\n", a + b);
+        
+    }
+}
