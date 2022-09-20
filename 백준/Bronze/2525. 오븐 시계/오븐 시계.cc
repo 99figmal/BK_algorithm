52b3b9329e01ea5f0c9d6332c028c3ffb9d044e8
99figmal <109018252+99figmal@users.noreply.github.com>
2022-09-21 00:17:28 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+
+int main(){
+    int h, m, c;
+    scanf("%d %d", &h, &m);
+    scanf("%d", &c);
+
+    if( m + c >= 60){
+        h = h + ((m + c)/ 60);
+        m = (m + c) % 60;
+        
+        if( h >= 24 ) h = h % 24;
+        
+        printf("%d %d", h, m);
+    }
+    else{
+        m = m + c;
+        printf("%d %d", h, m);
+    }
+
+}
+
+
+
